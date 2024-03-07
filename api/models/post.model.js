2d731853604b9820403437790b32430479993974
hitsantoki <21dcs106@charusat.edu.in>
2024-03-08 01:58:04 +0530

    add update post functionality

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,35 +1,38 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const postSchema =new mongoose.Schema({
-    userId:{
-        type : String,
-        required : true,
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-    content :{
-        type : String,
-        required : true,
+    content: {
+      type: String,
+      required: true,
     },
-    title :{
-        type : String,
-        required : true,
-        unique:true,
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    image :{
-        type: String,
-        default : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-7EPjTIhjCVY_zb5h1Ye0JDM4Fbf1K8CzAA&usqp=CAU',
+    image: {
+      type: String,
+      default:
+        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
     },
-    category : {
-        type: String,
-        default : 'uncategorized'
+    category: {
+      type: String,
+      default: 'uncategorized',
     },
-    slug : {
-        type : String,
-        required : true,
-        unique: true
-    }
-},{timestamps:true}
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
 );
 
-const Post  =mongoose.model('Post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 export default Post;
